dccfb8cda94d3cf9f5fd6bf53024eed05bb9c8f3
MRSantos1804 <marcosrodrigo.santos@gmail.com>
2026-09-11 18:52:03 -0300

    Adicionando rotas API

---
+++ mf-apis-web-services-fuel-manager_FWK6/Controllers/VeiculosController.cs
@@ -0,0 +1,52 @@
+﻿using mf_apis_web_services_fuel_manager_FWK6.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager_FWK6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.Veiculos.ToListAsync();
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Veiculo model)
+        {
+            if (model.AnoFabricacao <= 0 || model.AnoModelo <=0)
+            {
+                return BadRequest(new {message = "Ano de Fabricação e Ano do Modelos são obrigatórios e devem ser maiores que zero" });
+            }
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
+
+            return Ok(model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (model == null) NotFound();
+
+            return Ok(model);
+        }
+    }
+}
