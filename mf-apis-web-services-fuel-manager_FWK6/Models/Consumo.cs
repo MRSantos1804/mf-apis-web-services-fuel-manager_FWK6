f52e30ab472b5256a1b56a16b7f981eb5bfcd4e4
MRSantos1804 <marcosrodrigo.santos@gmail.com>
2026-09-10 22:41:08 -0300

    Inseridno modelos de tabelas

---
+++ mf-apis-web-services-fuel-manager_FWK6/Models/Consumo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager_FWK6.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Descricao { get; set; }
+        [Required]
+        public int Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Required]
+        public int VeiculoId { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+    }
+    public enum TipoCombustivel
+    {                        
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
