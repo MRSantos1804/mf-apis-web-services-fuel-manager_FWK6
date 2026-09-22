a24df1fbbcdeafac650ef5f6ecc181a0aef85ea6
MRSantos1804 <marcosrodrigo.santos@gmail.com>
2026-09-22 10:45:53 -0300

    alterações

--- mf-apis-web-services-fuel-manager_FWK6/Controllers/ConsumosController.cs
+++ mf-apis-web-services-fuel-manager_FWK6/Controllers/ConsumosController.cs
@@ -1,10 +1,12 @@
 ﻿using mf_apis_web_services_fuel_manager_FWK6.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace mf_apis_web_services_fuel_manager_FWK6.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ConsumosController : ControllerBase
---
+++ mf-apis-web-services-fuel-manager_FWK6/Models/AuthenticateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mf_apis_web_services_fuel_manager_FWK6.Models
+{
+    public class AuthenticateDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
